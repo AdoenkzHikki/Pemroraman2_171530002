90d52ce7f3f807a90a8c33f0e99e047d23f17390
AdoenkzHikki <37066436+AdoenkzHikki@users.noreply.github.com>
2018-08-28 21:37:39 +0700

    Add files via upload

---
+++ praktikum1/Profilktp.java
@@ -0,0 +1,22 @@
+public class Profilktp {
+
+    public static void main (String [] args) {
+        System.out.println("            PROVINSI JAWA TENGAH         ");
+        System.out.println("            KABUPATEN PEKALONGAN         ");
+        
+        System.out.println("NIK                 : 3326062212980003");
+
+        System.out.println("Nama                    :ABDUL KHAKIM");
+        System.out.println("Tempat/Tgl Lahir        :PEKALONGAN,22-12-1998");
+        System.out.println("Jenis Kelamin           :LAKI-LAKI      Gol.Darah:B");
+        System.out.println("Alamat                  :DUKUH KASRI");
+        System.out.println("    RT/RW               :08/04");
+        System.out.println("    Kel/Desa            :SAWANGAN");
+        System.out.println("    Kecamatan           :DORO");
+        System.out.println("Agama                   :ISLAM");
+        System.out.println("Status Perkawinan       :BELUM KAWIN");
+        System.out.println("Pekerjaan               :PELAJAR/MAHASISWA");
+        System.out.println("Kewarganegaraan         :WNI");
+        System.out.println("Berlaku Hingga          :SEUMUR HIDUP");
+    }
+}
---
+++ praktikum1/Profilmahasiswa.java
@@ -0,0 +1,14 @@
+public class Profilmahasiswa {
+
+    public static void main (String [] args) {
+        System.out.println("Nama                    :Abdul Khakim");
+        System.out.println("NIM                     :171530002");
+        System.out.println("Prodi                   :D2-Teknik Informatika");
+        System.out.println("Tempat/Tgl Lahir        :171530002");
+        System.out.println("Alamat                  :Dukuh Kasri");
+        System.out.println("    RT/RW               :08/04");
+        System.out.println("    Kel/Desa            :Sawangan");
+        System.out.println("    Kecamatan           :Doro");
+        System.out.println("Agama                   :Islam");
+    }
+}
