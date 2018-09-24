b8644a7f98731396d2c9cd78774c727a332e3446
AdoenkzHikki <37066436+AdoenkzHikki@users.noreply.github.com>
2018-09-24 10:52:38 +0700

    Add files via upload

---
+++ praktikum4/DemoIf.java
@@ -0,0 +1,10 @@
+public class DemoIf {
+
+    public static void main (String [] args) {
+        String nim = "171530002";
+
+        if (nim == "171530002"){
+            System.out.println ("Nama anda Abdul Khakim");
+        }
+    }
+}
